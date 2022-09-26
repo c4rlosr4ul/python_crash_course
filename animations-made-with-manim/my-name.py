9c13604463d1d18f76c623c946a87cf7e6376c63
r4ulc4rlos <carlosraul.crlsrl@gmail.com>
2022-09-25 21:23:51 -0500

    day_4-v.1.3

---
+++ animations-made-with-manim/my-name.py
@@ -0,0 +1,12 @@
+from manim import *
+
+class Testing(Scene):
+    def construct(self):
+        name=Tex("Carlos Raúl").to_edge(UR, buff=0.5)
+        sq=Square(
+            side_length=2, stroke_color=BLUE, fill_color=WHITE, fill_opacity=0.5 
+        ).shift(LEFT*3)
+        
+        self.play(Write(name))
+        self.play(DrawBorderThenFill(sq), run_time=3)
+        self.wait()
---
+++ try_it_yoursefl_and_more/contraseña.py
@@ -0,0 +1,10 @@
+psswrd = open('cona.txt')
+pssw = psswrd.read()
+print('Ingresa la contraseña rctmáre')
+contrarctmare = input()
+if contrarctmare == pssw:
+    print('Contraseña correcta')
+    if contrarctmare == '69':
+        print('Oe OpiO')
+else: 
+    print('Contraseña incorrecta')  
